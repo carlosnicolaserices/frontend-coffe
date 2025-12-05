ec9035dba6c87711822597222a9f822cd70e228c
Carlos Erices Mendez <carlos.erices@bci.cl>
2025-12-05 18:54:32 -0300

    primer commit del frontend

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cupcup.app',
+  appName: 'Cup Cup Route',
+  webDir: 'dist'
+};
+
+export default config;
